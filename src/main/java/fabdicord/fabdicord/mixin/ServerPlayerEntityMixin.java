f277d8fd33366afd591494307a7f649e5ca7d339
Nekouzki <106313126+Nekozuki2009@users.noreply.github.com>
2024-08-25 14:26:55 +0900

    discordからコマンド実行できるようにした。メンション可能か設定できるようにした。botも認識するようにした。コマンドを整理した

--- src/main/java/fabdicord/fabdicord/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/fabdicord/fabdicord/mixin/ServerPlayerEntityMixin.java
@@ -1,40 +1,39 @@
 package fabdicord.fabdicord.mixin;
 
-import io.netty.buffer.Unpooled;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import carpet.patches.EntityPlayerMPFake;
+import fabdicord.fabdicord.discord;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.network.PacketByteBuf;
-import net.minecraft.network.message.MessageType;
-import net.minecraft.network.message.SentMessage;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.nio.charset.StandardCharsets;
+import java.awt.*;
 
-import static fabdicord.fabdicord.Fabdicord.config;
+import static fabdicord.fabdicord.Fabdicord.ServerName;
 
 @Mixin(ServerPlayerEntity.class)
 public class ServerPlayerEntityMixin {
     @Inject(method = "onDeath", at = @At("TAIL"))
     public void onDeathInject(DamageSource source, CallbackInfo callbackInfo) {
         ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
-        //death type:server:player:dim:xyz:message
-        ServerPlayNetworking.send(
-                player,
-                new Identifier("velocity", "fabdicord"),
-                new PacketByteBuf(Unpooled.wrappedBuffer(("DEATH:" + config.get("SERVER") + ":" + player.getName().getString() + ":"
-                        + (player.getWorld().getRegistryKey() == World.OVERWORLD ? "OVERWORLD" : player.getWorld().getRegistryKey() == World.NETHER ? "NETHER" : "END") + ":"
-                        + "(" + ((int) player.getPos().x) + ", " + ((int) player.getPos().y) + ", " + ((int) player.getPos().z) + ")" + ":" + source.getDeathMessage(player).getString())
-                        .getBytes(StandardCharsets.UTF_8))));
-    }
+        String PN = player.getName().getString();
+        String place = (player.getWorld().getRegistryKey() == World.OVERWORLD ? "OVERWORLD" : player.getWorld().getRegistryKey() == World.NETHER ? "NETHER" : "END") + ":"
+                + "(" + ((int) player.getPos().x) + ", " + ((int) player.getPos().y) + ", " + ((int) player.getPos().z) + ")";
+        boolean isbot = player instanceof EntityPlayerMPFake;
+        String PNpre = isbot ? "(BOT) " : "";
+        String message = source.getDeathMessage(player).getString();
+        discord.sendMessage(new EmbedBuilder()
+                .setTitle("[" + ServerName + "] の [" + place + "] で死亡しました")
+                .setDescription(message)
+                .setColor(Color.red)
+                .setAuthor(PNpre + PN, null, "https://mc-heads.net/avatar/" + PN + ".png")
+                .build(), false);
 
-    @Inject(method = "sendChatMessage", at = @At("HEAD"), cancellable = true)
-    private void blockChat(SentMessage message, boolean filterMaskEnabled, MessageType.Parameters params, CallbackInfo ci) {
-        ci.cancel();
+        discord.PMChannel.sendMessage("VELOCITY&READ&<hover:show_text:'" + message + "'><red><dark_green>[" + ServerName + "]</dark_green><yellow>" + place
+                + "</yellow> で <aqua>[<blue>" + PNpre + "</blue>" + PN + "]</aqua> が死亡しました&" + ServerName + place + "で" + PNpre + PN + "が死亡しました").queue();
     }
 }
